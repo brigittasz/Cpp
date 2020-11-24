d69a82a1493ee3d55476b932981ff9a464833ae4
brigittasz <48497331+brigittasz@users.noreply.github.com>
2020-11-24 13:08:20 -0500

    Delete programstructure.cpp

--- programstructure.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-//c++ also allows to specify the library name using double quotes
-//#include "main.hpp"
-
-int main(){
-    std::cout << "Hello World, I am ready for c++";
-    return 0;
-}
