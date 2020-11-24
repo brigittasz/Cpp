9675ff9d76c3eb0ad53e747d24bb3b0e7a1480b9
brigittasz <48497331+brigittasz@users.noreply.github.com>
2020-11-24 13:08:05 -0500

    Create programstructure.cpp

---
+++ Lesson1/programstructure.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//c++ also allows to specify the library name using double quotes
+//#include "main.hpp"
+
+int main(){
+    std::cout << "Hello World, I am ready for c++";
+    return 0;
+}
