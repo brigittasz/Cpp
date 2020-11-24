28448f19872925b25504fdaf3b6b701231348fbb
brigittasz <48497331+brigittasz@users.noreply.github.com>
2020-11-24 13:12:47 -0500

    Update programstructure.cpp

--- Lesson1/programstructure.cpp
+++ Lesson1/programstructure.cpp
@@ -1,3 +1,6 @@
+ /*write a C++ program that outputs the following statement:
+ *** "Hello world, I am ready for C++"
+ */
 #include <iostream>
 //c++ also allows to specify the library name using double quotes
 //#include "main.hpp"
